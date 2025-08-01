24554abaf353d98701aa1cea664b50c1015341ff
elizabeth <leezeelola@gmail.com>
2025-08-01 10:21:56 +0100

    Add sitemap integration and update site URL in configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: "thea11ypath.com",
+  site: "thea11ysite.com",
   vite: {
     plugins: [tailwindcss()]
   },
 
+  integrations: [sitemap()],
 });
